cd1f372b8835159aed5420ee9cdb4f8c0840c392
HelioQuantumDev16 <adityasuryawanshi16@gmail.com>
2024-08-06 21:16:37 +0530

    first commit

---
+++ if_else_statement.cpp
@@ -0,0 +1,19 @@
+// C++ Program to check input number is positive or negative
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a Number";
+    cin>>n;
+
+    if(n>0)
+    {
+      cout<<"Number is Positive"<<endl;
+    }
+    else{
+      cout<<"Number is Negative"<<endl;
+    }
+    return 0;
+}

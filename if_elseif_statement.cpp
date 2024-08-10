a6beada216b8fdc2e32e2858a760b14403012347
HelioQuantumDev16 <adityasuryawanshi16@gmail.com>
2024-08-10 20:24:17 +0530

    adding if_elseif_statement file

---
+++ if_elseif_statement.cpp
@@ -0,0 +1,24 @@
+// Write a Program to check the input number is positive, negative or Zero.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a;
+  cout<<"Enter the value of a";
+  cin>>a;
+
+  if(a>0)
+  {
+    cout<<"A is Positive"<<endl;
+  }
+  else if(a<0)
+  {
+    cout<<"A is Negative"<<endl;
+  }
+  else
+  {
+    cout<<"A is Zero"<<endl;
+  }
+    return 0;
+}
